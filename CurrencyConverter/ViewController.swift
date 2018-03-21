5dacf0ecb5060b2a4f41a89242325f06dbe4ce74
almozaai <info@salehsaeed.com>
2018-03-21 21:59:09 +0300

    add some files

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let poundRate = 0.69
+    let yenRate = 113.94
+    let euroRate = 0.89
+    var dollarAmount = 0.0
 
+    @IBOutlet weak var poundLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.delegate = self
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            updateData()
+            return true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+            updateData()
     }
-
-
+        
+    }
+    
+    @IBAction func convertCurrency(_ sender: UIButton) {
+        updateData()
+    }
+    
+    @IBAction func clearCurrency(_ sender: UIButton) {
+        inputTextField.text = ""
+        poundLabel.text = "0.00"
+        yenLabel.text = "0.00"
+        euroLabel.text = "0.00"
+        
+    }
+    
+    func updateData() {
+        if let amount = Double(inputTextField.text!) {
+            dollarAmount = amount
+        }
+        
+        poundLabel.text = "\(poundRate * dollarAmount)"
+        yenLabel.text = "\(yenRate * dollarAmount)"
+        euroLabel.text = "\(euroRate * dollarAmount)"
+    }
+    
 }
 
